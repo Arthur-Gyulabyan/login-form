a24e3a56dec231b77f2d56720e1640d0c48b4ee6
Arthur <gyulabyana@gmail.com>
2021-07-20 10:24:17 +0400

    feat(helpers): created general verifier function

--- src/helpers/verifiers/verifiers.js
+++ src/helpers/verifiers/verifiers.js
@@ -23,6 +23,29 @@ export const isValidPassword = (psw) => {
   return psw.match(regEx);
 };
 
+export const isValidInput = (input, type) => {
+  const regExes = {
+    name: /^[a-zA-Z]+$/,
+    email: /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+    password: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/,
+  }
+
+  const lowerCaseType = type.toLowerCase();
+
+  switch (lowerCaseType) {
+    case 'firstname':
+      return regExes.name.test(input) && input.length > 1;
+    case 'lastname':
+      return regExes.name.test(input) && input.length > 3;
+    case 'email':
+      return regExes.email.test(input);
+    case 'password':
+      return regExes.password.test(input);
+    default:
+      throw new Error('Unknown type of input!');
+  }
+}
+
 export const isTheSamePassword = (psw, pswRepeat) => {
   return isValidPassword(pswRepeat) && psw === pswRepeat;
 };
