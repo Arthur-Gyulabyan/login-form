0d8778aad73bb01440fda07f13b270efab1dda0e
Arthur <gyulabyana@gmail.com>
2021-07-20 10:32:55 +0400

    feat(LoginInput): add onChange handlers and pass props to Input

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import Input from '../Input/Input';
 import {
-  isValidName,
-  isValidLastName,
-  isValidEmail,
-  isValidPassword,
+  isValidInput,
   isTheSamePassword,
 } from '../../helpers/verifiers/verifiers';
 
@@ -15,15 +12,86 @@ export default class LoginForm extends React.Component {
     this.state = {
       isValidFirstName: false,
       isValidLastName: false,
+      isValidEmail: false,
+      isValidPassword: false,
+      isValidPasswordRepeat: false,
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      passwordRepeat: '',
     };
   }
+
+  handleInputChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    isValidInput(value, name)
+      ? this.setState({
+          [`isValid${name}`]: true,
+          [`${name[0].toLowerCase() + name.slice(1)}`]: value,
+        })
+      : this.setState({ [`isValid${name}`]: false });
+  };
+
+  handlePasswordRepeatChange = (e) => {
+    const value = e.target.value;
+
+    isTheSamePassword(value, this.state.password)
+      ? this.setState({ isValidPasswordRepeat: true, passwordRepeat: value })
+      : this.setState({ isValidPasswordRepeat: false });
+  };
 
   render() {
     return (
       <div>
         <form>
-          <Input type="text" text="First Name" verifier={isValidName} />
-          <Input type="text" text="Last Name" verifier={isValidLastName} />
+          <Input
+            name="FirstName"
+            type="text"
+            text="First Name"
+            isValid={this.state.isValidFirstName}
+            validText="Valid"
+            noValidText="No Valid"
+            verifier={this.handleInputChange}
+          />
+          <Input
+            name="LastName"
+            type="text"
+            text="Last Name"
+            isValid={this.state.isValidLastName}
+            validText="Valid"
+            noValidText="No Valid"
+            verifier={this.handleInputChange}
+          />
+          <Input
+            name="Email"
+            type="email"
+            text="Email"
+            isValid={this.state.isValidEmail}
+            validText="Valid"
+            noValidText="No Valid"
+            verifier={this.handleInputChange}
+          />
+          <Input
+            name="Password"
+            type="password"
+            text="Password"
+            isValid={this.state.isValidPassword}
+            validText="Valid"
+            noValidText="No Valid"
+            verifier={this.handleInputChange}
+          />
+          <Input
+            name="PasswordRepeat"
+            type="password"
+            text="Repeat Password"
+            isValid={this.state.isValidPasswordRepeat}
+            validText="Valid"
+            noValidText="No Valid"
+            verifier={this.handlePasswordRepeatChange}
+          />
         </form>
       </div>
     );
